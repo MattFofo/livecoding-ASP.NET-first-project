9975b2ceb0ce392fefea8e2435959c0c0a59bb11
MattFofo <matteo.segattini2@gmail.com>
2022-07-13 14:03:22 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace livecoding_ASP.NET_first_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Post.cs
@@ -0,0 +1,19 @@
+﻿namespace livecoding_ASP.NET_first_project
+{
+    public class Post
+    {
+        public int Id = 0;
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public Post(string title, string content, string imageUrl)
+        {
+            Title = title;
+            Content = content;
+            ImageUrl = imageUrl;
+            Id = new Random().Next(1, 999999);
+        }
+    }
+}
